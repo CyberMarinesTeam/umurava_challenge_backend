7e2cf2e0ce0574f52ad3a20b2031de41ba6fa6fd
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-01-29 01:42:29 +0200

    adding the notification gateway

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserSchema } from './models/auth.model';
+import { Users, UserSchema } from './models/auth.model';
 import { JwtModule } from '@nestjs/jwt';
 import * as dotenv from 'dotenv';
 import { UserController } from './controllers/user.controller';
@@ -19,7 +19,7 @@ dotenv.config();
       secret: process.env.JWT_SECRET_KEY as string,
       signOptions: { expiresIn: '1d' },
     }),
-    ConfigModule
+    ConfigModule,
   ],
   controllers: [AuthController, UserController],
   providers: [AuthService, UserService, JwtStrategy, AuthGuard, RolesGuard],
